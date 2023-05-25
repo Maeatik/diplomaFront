fb8650407e7ca1b004126828c5dfcb77b07b9297
marat.sharafeev <sharafeev.marat@wb.ru>
2023-05-25 03:39:40 +0300

    add: reading input text

--- src/components/ContentEditor.vue
+++ src/components/ContentEditor.vue
@@ -1,30 +1,46 @@
-<script lang="ts" setup>
-import { ref } from 'vue'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { component as CKEditor } from '@ckeditor/ckeditor5-vue';
-
-const editorData = ref('')
-const editorConfig = ref({
+<template>
+    <div class="ck_editor">
+      <CKEditor :editor="ClassicEditor" v-model="editorData" :config="editorConfig" @input="handleEditorInput"></CKEditor>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import { defineProps, defineEmits, ref, watch } from 'vue'
+  import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+  import { component as CKEditor } from '@ckeditor/ckeditor5-vue';
+  
+  const props = defineProps({
+    modelValue: String
+  });
+  
+  const emit = defineEmits(['update:modelValue', 'getText']);
+  
+  const editorData = ref(props.modelValue || '');
+  const editorConfig = ref({
     toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
     heading: {
-        options: [
-            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-        ]
+      options: [
+        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+      ]
     }
-})
-</script>
+  });
+  
+  watch(editorData, () => {
+    emit('update:modelValue', editorData.value);
+    emit('getText', editorData.value); // Добавлено для срабатывания события getText при изменении текста
+  });
 
-<template>
-    <div class="ck_editor" >
-        <CKEditor :editor="ClassicEditor" v-model="editorData" :config="editorConfig"></ckeditor>
-    </div>
-</template>
-
-<style>
-.ck_editor {
+  function handleEditorInput() {
+  console.log(editorData.value)
+}
+  </script>
+  
+  <style>
+  .ck_editor {
     --ck-border-radius: 0.6rem;
     size: 100px;
-}
-</style>
+  }
+  </style>
+  
--- src/views/TextPage.vue
+++ src/views/TextPage.vue
@@ -1,47 +1,65 @@
 <template>
     <v-app class="app">
-        <v-app-bar app color="primary" dark>
-            <v-toolbar-title>TextGrabber</v-toolbar-title>
-            <v-spacer></v-spacer>
-
-            <v-btn to="account" class="saveButton">Сохранить</v-btn>
-            <v-btn to="account">Выйти</v-btn>
-        </v-app-bar>
-
-        <ContentEditor class="sticky-object"></ContentEditor>
-
-        
+      <v-app-bar app color="primary" dark>
+        <v-toolbar-title>TextGrabber</v-toolbar-title>
+        <v-spacer></v-spacer>
+  
+        <v-btn @click="save()" class="saveButton">Сохранить</v-btn>
+        <v-btn @click="checkSave()">Выйти</v-btn>
+      </v-app-bar>
+  
+      <ContentEditor v-model="content" class="sticky-object" @getText="getTextFromEditor"></ContentEditor>
     </v-app>
-</template>
-
-
-<script lang="ts">
-import ContentEditor from '../components/ContentEditor.vue';
-
-export default {
-    components: {
-        ContentEditor,
-        
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  import ContentEditor from '../components/ContentEditor.vue';
+  import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+  
+  export default {
+    data() {
+      return {
+        saved: false,
+        overlayDelete: false,
+        overlayPassword: false,
+        content: '11231312' // Исправлено
+      };
     },
-}
-</script>
-
-<style>
-.container {
+    components: {
+      ContentEditor
+    },
+    methods: {
+      save() {
+        console.log('Текст изменен:', this.content);
+        this.saved = true;
+      },
+      checkSave() {
+        // Проверка сохранения
+      },
+      getTextFromEditor(text) {
+        this.content = text 
+        console.log('Текст из редактора:', text);
+        // Выполните необходимые действия с полученным текстом
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .container {
     display: flex;
     flex-direction: column;
     height: 100vh;
-}
-
-.content {
+  }
+  
+  .content {
     flex: 1;
-}
-
-.sticky-object {
+  }
+  
+  .sticky-object {
     margin-top: 70px;
     margin-inline: 100px;
     /* Опустить объект вниз */
-}
-
-
-</style>
+  }
+  </style>
